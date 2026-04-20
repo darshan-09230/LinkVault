e7189b480179c38d51c778cfb8df626c0d0eca27
darshan-09230 <darshandoshi092@gmail.com>
2026-04-20 18:20:59 +0530

    initial commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+import re
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+def extract_url(text):
+    pattern = r"(https?://[^\s]+)"
+    match = re.search(pattern, text)
+    return match.group(0) if match else None
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    url = extract_url(message.content)
+
+    if url:
+        await message.channel.send("✅ Link received!")
+
+client.run(TOKEN)
